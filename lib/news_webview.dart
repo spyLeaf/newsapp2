7a8cac08862a787ca0ffc91b1f29cf7b8e65088c
spyLeaf <soumyodeb@gmail.com>
2023-11-24 14:16:08 +0530

    1st commit

---
+++ lib/news_webview.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class NewsWebView extends StatefulWidget {
+  String url;
+  NewsWebView({super.key, required this.url});
+
+  @override
+  State<NewsWebView> createState() => _NewsWebViewState();
+}
+
+class _NewsWebViewState extends State<NewsWebView> {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: widget.url,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: Text("NEWS NOW"),
+      ),
+    );
+  }
+}
